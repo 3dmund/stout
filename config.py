bc8427845d663f664e1d19e32bf55445cb60da3c
Edmund Tian <edmundtian@ezt.local>
2021-11-25 19:03:23 -0500

    working end to end

--- config.py
+++ config.py
@@ -15,7 +15,8 @@ class Config(object):
     PELM_API_URL = "https://api.portertech.io"
     STOUT_URL = "https://porter-stout.herokuapp.com"
     PORTER_AUTH_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzY3NDkwOTcsImlhdCI6MTYzNjY2MjY5Nywic3ViIjo5fQ.XYb1Wzw81a9BTKOrDkV3UAM97giTQmznHzToCaKaug8"
-
+    PELM_CLIENT_ID = "123"
+    PELM_CLIENT_SECRET = "456"
 
 class ProductionConfig(Config):
     pass
@@ -29,7 +30,8 @@ class DevelopmentConfig(Config):
     PELM_API_URL = "http://0.0.0.0:5000"
     STOUT_URL = "http://0.0.0.0:100"
     PORTER_AUTH_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Mzc0MjcyMDgsImlhdCI6MTYzNzM0MDgwOCwic3ViIjoxMH0.glHctUmKAVDf3siZAcNKmpZ0WoKPpskIpa8CT93gEO4"
-
+    PELM_CLIENT_ID = "5008d06a-4e44-11ec-ace3-acde48001122"
+    PELM_CLIENT_SECRET = "c561cf481f6c5924e3066576928488125a2180d8a6ce08a125e4de08eebd4d35"
 
 class TestingConfig(Config):
     TESTING = True
--- project/main.py
+++ project/main.py
@@ -7,10 +7,14 @@ from flask_login import login_required, current_user
 from project import app
 from requests import post, get
 
+import base64
+
 main = Blueprint('main', __name__)
 
 EMAIL = 'stout@gmail.com'
 PASSWORD = 'password'
+
+BASE_64_ENCODED_STRING = 'NTAwOGQwNmEtNGU0NC0xMWVjLWFjZTMtYWNkZTQ4MDAxMTIyOmM1NjFjZjQ4MWY2YzU5MjRlMzA2NjU3NjkyODQ4ODEyNWEyMTgwZDhhNmNlMDhhMTI1ZTRkZTA4ZWViZDRkMzU='
 
 @main.route('/')
 @login_required
@@ -25,10 +29,49 @@ def index():
     # TODO: query unique_account_ids here
     # unique_account_ids = ['123', '456']
 
-    energy_account_ids = get_energy_accounts(unique_id)
+    access_token = request.args.get('access_token')
+
+    energy_account_ids = get_energy_accounts(unique_id, access_token)
+
+    print(f"energy_account_ids: {energy_account_ids}")
+
+    return render_template('index.html', unique_id=unique_id, unique_account_ids=energy_account_ids, access_token=access_token)
 
 
+@app.route('/data', methods=['GET'])
+def get_data():
+    print("getting data")
 
+    account_id = request.args.get('unique_account_id')
+
+    print("unique_account_id: {}".format(account_id))
+
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+
+    access_token = request.args.get('access_token')
+
+    url = '{PELM_API_URL}/intervals/get?account_id={account_id}&start_date={start_date}&end_date={end_date}'.format(
+        PELM_API_URL=app.config.get('PELM_API_URL'),
+        account_id=account_id,
+        start_date=start_date,
+        end_date=end_date
+    )
+
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token)
+    }
+
+    print(url)
+
+    response = get(url=url, headers=headers)
+
+    print(response)
+    print(response.json())
+
+    # return render_template('home.html', unique_id=unique_id, unique_account_ids=unique_account_ids)
+
+    return response.text
 
 
 
@@ -46,7 +89,7 @@ def connect_utility():
 @main.route('/pelm/authorize')
 def connect_utility_post():
     print("authorizing user")
-    url = "{host}/auth/consumer".format(host=app.config.get('PELM_API_URL'))
+    url = "{host}/auth/authorize".format(host=app.config.get('PELM_API_URL'))
     print(url)
 
     # headers = {
@@ -58,13 +101,12 @@ def connect_utility_post():
     # print(type(response))
     # # print(response.get_data)
 
-    token = get_pelm_auth_token()
-    print(f"token: {token}")
+    # token = get_pelm_auth_token()
+    # print(f"token: {token}")
 
-    url_with_params = "{url}?auth_token={auth_token}&redirect_url={redirect_url}".format(
+    url_with_params = "{url}?client_id={client_id}".format(
         url=url,
-        auth_token=get_pelm_auth_token(),
-        redirect_url="{}/stout/redirect".format(app.config.get("STOUT_URL"))
+        client_id=app.config.get('PELM_CLIENT_ID')
     )
 
     return redirect(url_with_params)
@@ -72,12 +114,19 @@ def connect_utility_post():
 @main.route('/redirect', methods=['GET'])
 def pelm_redirect():
     print("hit redirect")
+    print(request.args)
     unique_id = request.args.get('unique_id')
+    code = request.args.get('code')
     print(f"unique_id: {unique_id}")
 
-    redirect_url = '{url}?unique_id={unique_id}'.format(
+    # TODO: we're passing this along for now. Should save to db.
+    access_token = get_access_token(code)
+    print(access_token)
+
+    redirect_url = '{url}?unique_id={unique_id}&access_token={access_token}'.format(
         url=url_for('main.index'),
-        unique_id=unique_id
+        unique_id=unique_id,
+        access_token=access_token
     )
 
     # return unique_id
@@ -104,24 +153,62 @@ def get_pelm_auth_token():
     return response_json.get('auth_token')
 
 
-def get_energy_accounts(unique_id):
+def get_energy_accounts(unique_id, access_token):
     print("get energy accounts")
-    url = '{PELM_API_URL}/accounts/get?unique_id={unique_id}&auth_token={auth_token}'.format(
+    url = '{PELM_API_URL}/accounts/get?unique_id={unique_id}'.format(
         PELM_API_URL=app.config.get('PELM_API_URL'),
         unique_id=unique_id,
-        auth_token=get_pelm_auth_token()
     )
 
-    # headers = {
-    #     'Authorization': 'Basic {}'.format(get_pelm_auth_token())
-    # }
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token)
+    }
 
-    response = get(url)
+    response = get(url, headers=headers)
 
     print("response")
     print(response)
     print(response.json())
     print(response.content)
 
-    return response.content
+    return response.json()['data']
+
+def get_access_token(code):
+    print("getting access_token")
+    url = '{PELM_API_URL}/auth/token'.format(
+        PELM_API_URL=app.config.get('PELM_API_URL')
+    )
+
+    data = {
+        'code': code,
+        'redirect_url': '{}/redirect'.format(app.config.get('STOUT_URL'))
+    }
+
+
+    string = '{id}:{secret}'.format(
+        id = app.config.get('PELM_CLIENT_ID'),
+        secret=app.config.get('PELM_CLIENT_SECRET')
+    )
+    encoded_string = base64.b64encode(string.encode())
+
+    headers = {
+        'Authorization': f'Basic {BASE_64_ENCODED_STRING}'
+    }
+
+    response = post(url, data=data, headers=headers)
+
+    print(response)
+
+    print(response.text)
+
+    data = response.json()['data']
+
+    access_token = data['access_token']
+    refresh_token = data['refresh_token']
+
+    print(access_token)
+    print(refresh_token)
+
+    return access_token
+
 
